48fe0028b74a396e993d143e82012f2363a95ffe
Jason Fukura <jason@jasonfukura.com>
2016-08-20 20:02:04 -0700

    Setting things up and preparing for the future.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This will be a Single Page App and will need to be constructed using MVC and
+ * OO principals.  For simplicity, it should be flat-file
+ *
+ * TODO: Setup the schema for controllers and models
+ *
+ * TODO: Log the goals of the application
+ * Before Christmas:
+ * 		Users visit, see paperchain items (a la Starbucks holiday free coffee campaign)
+ * 			Each day user sees today's item
+ * 			User can see previous items, but not future items
+ * On Christmas:
+ * 		Year highlights available
+ * On New Year's Eve @midnight:
+ * 		Video is available (Year in review video)
+ *
+ * Users can:
+ * 		Sign up for mailing list -> updates emailed to them
+ * 		Share items or a link?
+ *
+ * Password protected and public versions?
+ *
+ * TODO: Prototype.  The application itself should be view agnostic (as much as possible)
+ *
+ */
+
+?>
